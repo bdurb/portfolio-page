84b659e130ff0c22c1aaa6e3661bcfeaaff6f2e1
bdurb <brian@rpmgreenville.com>
2018-03-15 10:36:55 -0400

    changed for name

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header className="header">
     <div className="header__content">
-      <h1>Portfolio</h1>
+      <h1>Brian Durbin</h1>
       <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
       <NavLink to="/portfolio" activeClassName="is-active" exact={true}>Portfolio</NavLink>
       <NavLink to="/contact" activeClassName="is-active">Contact</NavLink>
