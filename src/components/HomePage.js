972c12599d57f03b8d50a53bb01e29ce35561020
bdurb <brian@rpmgreenville.com>
2018-03-15 10:34:57 -0400

    working with styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 const HomePage = () => (
   <div className="box-layout">
-    <div className="box-layout__picture">
-      </div>
       <h1>Welcome</h1>
       <p>This is my site</p>
   </div>
